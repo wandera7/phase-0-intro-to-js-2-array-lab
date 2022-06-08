85e9bf1b6f6f0f74b1f07f38b351316b7ecb1b58
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-08 08:37:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function  destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(name){
+    const newCats=cats.slice(0);
+    newCats.push(name);
+    return newCats;
+}
+function prependCat(name){
+    const newCats=cats.slice(0);
+    newCats.unshift(name);
+    return newCats;
+}
+function removeLastCat(){
+    let newCats=[...cats];
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat(){
+    const newCats=cats.slice(0);
+    newCats.shift();
+    return newCats;
+}
